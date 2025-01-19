9719934b0e32f2e455724ffbe9b2d7ee24d1d5b5
romanlysyk <roma-lysyk@ukr.net>
2025-01-20 00:47:03 +0200

    fix

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -3,15 +3,21 @@ import FeaturedProducts from '@/components/Home/FeaturedProducts';
 import Banner from '@/components/Home/Banner';
 import TextSeo from '@/components/Home/TextSeo';
 
-export default async function Home({ params: { locale }, }: Readonly<{ params: { locale: string } }>) {
+interface HomeProps {
+  params: {
+    locale: string;
+  };
+}
+
+export default async function Home({ params: { locale } }: HomeProps) {
   return (
     <main>
       <Filter />
-      <div className='container mx-auto px-4 py-5 min-h-[70vh]'>
-        <FeaturedProducts locale={ locale } />
-        <Banner locale={ locale } />
-        <TextSeo locale={ locale } />
+      <div className="container mx-auto px-4 py-5 min-h-[70vh]">
+        <FeaturedProducts locale={locale} />
+        <Banner locale={locale} />
+        <TextSeo locale={locale} />
       </div>
     </main>
   );
-};
+}
