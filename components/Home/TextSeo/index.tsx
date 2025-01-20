916021997da586e97489ce828b17d2b9071c84c4
romanlysyk <roma-lysyk@ukr.net>
2025-01-20 23:00:11 +0200

    fix seo text

--- components/Home/TextSeo/index.tsx
+++ components/Home/TextSeo/index.tsx
@@ -1,13 +1,16 @@
 import { FC } from 'react';
 import DOMPurify from 'isomorphic-dompurify';
+import { JSDOM } from "jsdom";
 
 interface Props {
 	description: string;
 }
 
 const TextSeo: FC<Props> = ({ description })=> {
+	console.log(description)
+	
 	const HtmlContent = ({ htmlString }: { htmlString: string }) => {
-		const sanitizedHtml = DOMPurify.sanitize(htmlString);
+		const sanitizedHtml = DOMPurify((new JSDOM("<!DOCTYPE html>")).window).sanitize(htmlString);
 		return (
 			<div
 				className="container mx-auto max-w-7xl mt-20 mb-24 px-2"
