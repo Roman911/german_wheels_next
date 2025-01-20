f4e9b47d73e77e15f34d242f108f0ea29385f44b
romanlysyk <roma-lysyk@ukr.net>
2025-01-21 00:26:13 +0200

    Added products to home page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,7 +1,9 @@
 import Filter from '@/components/Home/HomeFilter';
-// import FeaturedProducts from '@/components/Home/FeaturedProducts';
 import Banner from '@/components/Home/Banner';
 import TextSeo from '@/components/Home/TextSeo';
+import Title from '@/components/Lib/Title';
+import ProductList from '@/components/ProductList';
+import NoResult from '@/components/Lib/NoResult';
 
 async function getSettings() {
   const res = await fetch('https://admin.g-wheels.com.ua/baseData/settings', {
@@ -13,17 +15,33 @@ async function getSettings() {
   return await res.json();
 }
 
+async function getProducts() {
+  const res = await fetch('https://admin.g-wheels.com.ua/api/getProducts?vehicle_type=1&order[value]=popular&order[asc]=0', {
+    method: 'GET',
+    headers: {
+      'Access-Control-Allow-Credentials': 'true',
+    }
+  });
+  return await res.json();
+}
+
 export default async function Home({ params, }: { params: Promise<{ locale: string }> }) {
   const locale = (await params).locale;
   const response = await getSettings();
+  const products = await getProducts();
 
-  // console.log(locale, response);
+  console.log(locale, response, products);
 
   return (
     <main>
       <Filter />
       <div className="container mx-auto px-4 py-5 min-h-[70vh]">
-        {/*<FeaturedProducts />*/}
+        <Title title={ response[locale].h2_top }/>
+        { products.result ? <ProductList
+          locale={ locale }
+          classnames='grid-cols-1 md:grid-cols-2 lg:grid-cols-4 2xl:grid-cols-5'
+          data={ products.data }
+        /> : <NoResult noResultText='no result' /> }
         <Banner />
         <TextSeo description={response[locale].description} />
       </div>
