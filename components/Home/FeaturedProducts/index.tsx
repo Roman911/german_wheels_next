e3ef0eb786f68bdb6b616fc75ec10677f27b078d
romanlysyk <roma-lysyk@ukr.net>
2025-01-20 22:23:19 +0200

    fix

--- components/Home/FeaturedProducts/index.tsx
+++ components/Home/FeaturedProducts/index.tsx
@@ -3,7 +3,6 @@ import { useTranslations } from 'next-intl';
 import { baseDataAPI } from '@/services/baseDataService';
 import { useLanguages } from '@/hooks/language';
 import Title from '@/components/Lib/Title';
-import Spinner from '@/components/Lib/Spinner';
 import ProductList from '@/components/ProductList';
 import NoResult from '@/components/Lib/NoResult';
 
@@ -13,7 +12,6 @@ const FeaturedProducts = () => {
 	const { data: dataSetting } = baseDataAPI.useFetchSettingsQuery('');
 	const {
 		data,
-		isLoading
 	} = baseDataAPI.useFetchProductsQuery({ id: '?vehicle_type=1&order[value]=popular&order[asc]=0' });
 	
 	return (
