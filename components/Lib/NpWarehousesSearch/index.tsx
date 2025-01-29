00f8f11526ab9f5cd88dfb7f3bb5685902ba6756
romanlysyk <roma-lysyk@ukr.net>
2025-01-29 23:55:53 +0200

    fix

--- components/Lib/NpWarehousesSearch/index.tsx
+++ components/Lib/NpWarehousesSearch/index.tsx
@@ -1,22 +1,23 @@
 import { FC } from 'react';
 
-import { baseDataAPI } from '../../../services/baseDataService';
-import { useAppDispatch, useAppSelector } from '../../../hooks';
-import { setWirehouse } from '../../../store/reducers/orderSlice';
-import { MySelect } from '../../../components/Order/Select';
-import { Language } from '../../../models/language';
+import { baseDataAPI } from '@/services/baseDataService';
+import { useAppDispatch, useAppSelector } from '@/hooks/redux';
+import { setWirehouse } from '@/store/slices/orderSlice';
+// import { Language } from '@/models/language';
+import MySelect from '@/components/Lib/Select';
 
 interface NpWarehousesSearchProps {
 	title: string
 }
 
 export const NpWarehousesSearch: FC<NpWarehousesSearchProps> = ({ title }) => {
-	const { orderReducer: { city }, langReducer: { lang } } = useAppSelector(state => state);
+	const { city } = useAppSelector(state => state.orderReducer);
 	const dispatch = useAppDispatch();
 	const { data } = baseDataAPI.useFetchNpWarehousesQuery(city.value);
 
 	const warehousesOptions = data?.map((item: { Description: string, DescriptionRu: string, Ref: string }) => {
-		return { value: item.Ref, label: lang === Language.UA ? item.Description : item.DescriptionRu }
+		console.log(item)
+		// return { value: item.Ref, label: lang === Language.UA ? item.Description : item.DescriptionRu }
 	});
 
 	const onChange = (_: string, value?: number | string, label?: number | string) => {
