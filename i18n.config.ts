82b25de06f0a200df855bcb1af254159089c9169
romanlysyk <roma-lysyk@ukr.net>
2025-02-03 15:47:57 +0200

    added default domain

--- i18n.config.ts
+++ i18n.config.ts
@@ -3,6 +3,12 @@ import { Language } from './models/language';
 const i18nConfig = {
 	locales: [Language.UA, Language.RU],
 	defaultLocale: Language.UA,
+	domains: [
+		{
+			domain: 'http://shop.g-wheels.com.ua',
+			defaultLocale: Language.UA,
+		},
+	]
 };
 
 export default i18nConfig;
--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,15 @@
 import createMiddleware from 'next-intl/middleware';
+import { Language } from '@/models/language';
 
 export default createMiddleware({
 	locales: ['ua', 'ru'],
-	defaultLocale: 'ua'
+	defaultLocale: 'ua',
+	domains: [
+		{
+			domain: 'http://shop.g-wheels.com.ua',
+			defaultLocale: Language.UA,
+		},
+	]
 });
 
 export const config = {
