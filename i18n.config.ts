8cd543e11d0cf05430c66bea97e61420ab16f77e
romanlysyk <roma-lysyk@ukr.net>
2025-02-11 21:38:25 +0200

    fix locale

--- i18n.config.ts
+++ i18n.config.ts
@@ -3,13 +3,6 @@ import { Language } from './models/language';
 const i18nConfig = {
 	locales: [Language.UK, Language.RU],
 	defaultLocale: Language.UK,
-	localeDetection: false,
-	domains: [
-		{
-			domain: 'http://shop.g-wheels.com.ua',
-			defaultLocale: Language.UK,
-		},
-	]
 };
 
 export default i18nConfig;
